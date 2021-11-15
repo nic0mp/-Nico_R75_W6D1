c419eed66ee8da372c62013ec6b0007684a9f793
nic0mp <nico.mp215@gmail.com>
2021-11-15 16:48:32 -0600

    Homework

--- D1HW/js/main.js
+++ D1HW/js/main.js
@@ -3,25 +3,32 @@
 the list and checks that the current name is in the string passed in. The output should be:
 "Matched dog_name" if name is in the string, if no matches are present console.log "No Matches"
 */
+
 let dog_string = "Hello Max, my name is Dog, and I have purple eyes!"
 let dog_names = ["Max","HAS","PuRple","dog"]
 
-function findWords(){
-    //Your code goes here
-}
-
-//Call method here with parameters
+function findWords(nList){
+    for(let i =0; i < nList.length; i++){
+        console.log(nList[i])
+       
+    }
+   
+console.log(findWords(dog_names))
 
 //============Exercise #2 ============//
 /*Write a fucntion that takes in an array and removes every even index with a splice,
 and replaces it with the string "even index" */
 
-Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+let givenArr = ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
 
-function replaceEvens(arr){
+function replaceEvens(replacement){
     //code goes here
+    for(let i=0; i < givenArr.length; i++){
+        (i % 2 == 0)? givenArr.splice(i,1,replacement) : givenArr[i]
+    }
+    return givenArr
 }
-
+console.log(replaceEvens("even index"))
 //Expected output
 //Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
 //Output arr == ["even index","Baseball","even index","Goku","even index","Rodger"]
